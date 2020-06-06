6f6a348614ce3a56cff22c0e7876b8ef76099319
unknown <mazenashraf@Live.com>
2020-06-06 13:29:20 +0200

    Basic c file without timer module with defining

---
+++ RH850.h
@@ -0,0 +1,52 @@
+/*
+ * RH850.h
+ *
+ *  Created on: Jun 2, 2020
+ *      Author: Mazon saloma Ayoya
+ */
+
+#ifndef RH850_H_
+#define RH850_H_
+
+#define TAUB0I0 0XFFC30000 //Base address TAUB0
+#define INTTAUB0I0 0xFFC30044 //0XFFC30000 added 44 offset from table
+#define EIC17 0xFFFEEA22 //0xFFFEA00 and we added (17*2) offset which equals 0x22
+#define IMR0 0xFFFEEAF0 //base address of interrupt mask register
+//#define OSTM0CMP 0XFFEC0000 //32bit register of OSTMn Compare Register
+
+#define OSTMO 0
+#define OSTM1 1
+#define OSTM2 2
+#define OSTM3 3
+#define OSTM4 4
+#define OSTM5 5
+#define OSTM6 6
+#define OSTM7 7
+#define OSTM8 8
+#define OSTM9 9
+#define OSTM10 10
+#define OSTM11 11
+#define OSTM12 12
+#define OSTM13 13
+#define OSTM14 14
+#define OSTM15 15
+#define OSTM16 16
+#define OSTM17 17
+#define OSTM18 18
+#define OSTM19 19
+#define OSTM20 20
+#define OSTM21 21
+#define OSTM22 22
+#define OSTM23 23
+#define OSTM24 24
+#define OSTM25 25
+#define OSTM26 26
+#define OSTM27 27
+#define OSTM28 28
+#define OSTM29 29
+#define OSTM30 30
+#define OSTM31 31
+
+
+
+#endif /* RH850_H_ */
