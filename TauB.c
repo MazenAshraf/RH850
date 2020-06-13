2426989f09c5ca8407fae38ec85f1033b8d38672
Aya-MH <36451073+Aya-MH@users.noreply.github.com>
2020-06-13 16:40:44 +0200

    Add files via upload

---
+++ TauB.c
@@ -0,0 +1,43 @@
+/*
+ * TauB.c
+ *
+ *  Created on: Jun 8, 2020
+ *      Author: Aya
+ */
+
+#include "Rh850.h"
+#include "Common_Macros.h"
+#include "std_types.h"
+
+void TAUB_init(void)
+{
+  /*Tau prescalar setup */
+  TAUB0TPS = 0xFFF4u;     /* Set the TAUB0 prescalar CK0=PCLK/16 */
+
+  /*setting Counter Mode*/
+  TAUB0CMOR0 = 0x0001u;      /*specifies the counter operation mode to interval timer mode*/
+
+  /*and by setting TAUB0CMOR0.TAUB0MD0 Bit =1 means Specifies that INTTAUB0Im is output at the
+beginning of count operation (when a start
+trigger is entered)*/
+
+  /*setting rising edge detection*/
+  TAUB0CMUR0 = 0x01u;
+
+  /*enabling counter*/
+  SET_BIT(TAUB0TS,0);     /*setting bit by 1: Enables the counter and sets TAUBnTE.TAUBnTEm = 1 */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
